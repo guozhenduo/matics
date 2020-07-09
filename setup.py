2c8a400a60aa4bb8c7b0a77bc18842998b8e9dbb
suchenguo <guozhenduo1@163.com>
2020-07-09 18:41:51 +0800

    升级matics

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
 	name='matics',
-	version="1.3.2rc1",
+	version="1.3.2rc2",
 	keyword=['math','mathematics'],
 	author='suchenguo',
 	author_email='guozhenduo1@163.com',
 	description="一个小学数学知识的模块",
-	packages=find_packages
+	packages=find_packages()
 )
