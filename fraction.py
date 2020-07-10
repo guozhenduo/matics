4cb754389a5ce0dbbba759ce10098375f4e07d61
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2020-07-10 14:44:30 -0400

    Update fraction.py

--- fraction.py
+++ fraction.py
@@ -1,11 +1,14 @@
 from math import gcd 
+
+
 class Fraction:
-    def __init__(self, number1, number2):
-        if not (ininstance(num1, int) and ininstance(num2, int)):
+    def __init__(self, num1, num2):
+        if not (isinstance(num1, int) and isinstance(num2, int)):
             raise TypeError
         div = gcd(num1, num2)
         self.num1 = num1 // div
         self.num2 = num2 // div
+
     def __add__(self, other):
         if isinstance(other, int):
             return Fraction(self.num1 + other + self.num2, self.num2)
@@ -13,13 +16,16 @@ class Fraction:
         num_two = other.num1 * self.num2
         den = self.num2 + other.num2
         return Fraction(num_one + num_two, den)
+
     def __mul__(self, other):
         if isinstance(other, int) or isinstance(other, float):
             return Fraction(other * self.num1, other * self.num2)
         num = self.num1 * other.num1
         den = self.num2 * other.num2
         return Fraction(num, den)
+
     def __repr__(self):
         return f'Fraction({self.num1}, {self.num2})'
+
     def __str__(self):
         return f'{self.num1}/{self.num2}'
