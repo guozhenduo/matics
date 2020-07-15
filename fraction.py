7cdb76ad2918e3e78fd5b681dcc1c43ff76530d2
suchenguo <65109015+guozhenduo@users.noreply.github.com>
2020-07-15 09:39:08 +0800

    Update fraction.py

--- fraction.py
+++ fraction.py
@@ -3,25 +3,40 @@ import re
 
 
 class Fraction:
+
     def __init__(self, num1=0, num2=1):
+
         if not ({type(num1), type(num2)} - {int, float, str}):
+
             if type(num1) != str and type(num2) == str:
                 raise TypeError
+
         if isinstance(num1, str):
+
             regex = '-?[0-9]+(.[0-9]+)?'
+
             if re.match(f'^{regex}/{regex}$', num1):
+
                 num1, num2 = num1.split('/')
                 num1 = [int, float]['.' in num1](num1)
                 num2 = [int, float]['.' in num2](num2)
+
             else:
+
                 raise TypeError
+
         if not num2:
+
             raise ZeroDivisionError
+
         if isinstance(num1, int) and isinstance(num2, int):
+
             div = math.gcd(num1, num2)
             self.num1 = num1 // div
             self.num2 = num2 // div
+
         else:
+
             num2 = num2 if num1 else 1
             self.num1 = [float, int][num1 == int(num1)](num1)
             self.num2 = [float, int][num2 == int(num2)](num2)
