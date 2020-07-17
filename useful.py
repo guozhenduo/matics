8e432e5bb6e2590d29342ae6a8055d782d1ab680
suchenguo <65109015+guozhenduo@users.noreply.github.com>
2020-07-17 12:19:46 +0800

    Update useful.py

--- useful.py
+++ useful.py
@@ -7,25 +7,12 @@ try:
     #来源:未知
     def step_add(m):
         if type(m) != int:
-            print('类型必须为浮点数')
+            print('类型必须为整数')
             exit()
         if m <= 65535:
             return so.step_add(m)
         else:
-            return m * (m + 1) // 2
-    def step_mul(step_max):
-        l = (i for i in range(1, step_max + 1))
-        return reduce(lambda x, y: x * y, l)
-    #程序员:苏晨果
-    #1.0.2版
-    #来源:terminal
-    def tenimal():
-        while True: 
-            l = input('>>> ')
-            exec(l)
-            if l == ' ':
-                break
-                exit()
+            return m + (m+1) // 2
 
 except:
     print("Please inspect")
