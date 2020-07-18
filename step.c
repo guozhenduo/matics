579c079fbda754b56648d9447210f96a55d6ab6f
suchenguo <guozhenduo1@163.com>
2020-07-18 18:13:08 +0800

    step.c and step.cpp

--- step.c
+++ step.c
@@ -4,13 +4,7 @@
 
 long long step_add(long long max)  //max must be float
 {	
- 	long long j=0; //define j
- 	for (long long i=0;i<max+1;i+=1)  //a easy for loop
- 	{
- 		j+=i;  //Plus
- 	}
- 	
- 	return j;  //return j
+	return max * (max+1) / 2 ;
 }
 
 
--- step.cpp
+++ step.cpp
@@ -3,13 +3,7 @@
 
 long double step_add(long double max)  //max must be float
 {	
- 	long double j=0.0; //define j
- 	for (long double i=0.0;i<max+1;i+=1)  //a easy for loop
- 	{
- 		j+=i;  //Plus
- 	}
- 	
- 	return j;  //return j
+ 	return max * (max+1) / 2;
 }
 
 int main()
