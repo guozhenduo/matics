5e8427c0a6d906b5305a4e78ce9bc9b896901dd5
suchenguo <guozhenduo1@163.com>
2020-07-23 15:46:29 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,7 @@ setup(
 	author='suchenguo',
 	author_email='guozhenduo1@163.com',
 	description="一个包含矩阵，分数，计算，判断数类型，常量，方程的数学模块",
-	packages=find_packages()
+	packages=find_packages(),
+	license="MIT lincense",
+	install_requires=["Cython>=0.29.21"]
 )
