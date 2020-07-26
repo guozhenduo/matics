1f3a21906e5f254e782f0b3826dfeca66650efbf
suchenguo <guozhenduo1@163.com>
2020-07-26 14:24:15 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as re:
+    long_description = re.read()
+    
 setup(
 	name='matics',
 	version="1.3.2b2",
@@ -9,5 +12,8 @@ setup(
 	description="一个包含矩阵，分数，计算，判断数类型，常量，方程的数学模块",
 	packages=find_packages(),
 	license="MIT lincense",
-	install_requires=["Cython>=0.29.21"]
+	install_requires=["Cython>=0.29.21"],
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	scripts=["main.py","matrices.py","statistics.py","class1.py","class2.py","func1setup.py"]
 )
