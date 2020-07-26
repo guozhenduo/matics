3ed11ff11c955fc67c24f396201e2ac182b8f299
suchenguo <guozhenduo1@163.com>
2020-07-26 13:35:21 +0800

    Rewrite func1setup.py

--- func1setup.py
+++ func1setup.py
@@ -1,5 +1,7 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
-setup(name='matics func1  app',
-      ext_modules=cythonize("func1.pyx"))
+setup(
+    name='func1.pyx',
+    ext_modules=cythonize("func1.pyx")
+)
