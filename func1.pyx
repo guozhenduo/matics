1c250c4731c924476419ea1710fba12b3946b69b
suchenguo <guozhenduo1@163.com>
2020-07-26 13:30:32 +0800

    Keep Space

--- func1.pyx
+++ func1.pyx
@@ -1,17 +1,23 @@
 def composite(s):
 	list_a = []
 	for i in range(2, s + 1):
+	
 		for j in range(2, i):
+		
 			if i % j == 0 :
 				list_a.append(i)
 				break
+				
 	return list_a
+	
 def primer(se):
 	primes = [2, 3, 5, 7]
 	r = primes[:]
 	for a in range(8, int(se) + 1):
+	
 		if any(a % k == 0 for k in primes):
 			continue
+			
 		r.append(a)
 	return r
 
