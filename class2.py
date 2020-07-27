2276e7bd7e27cfc94b53b40accf8cfd8a12fdacc
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2020-07-27 18:25:40 -0400

    n-Dimensional Volume Class
    - Instead of having a class for each number of dimensions (2D and 3D), one can create a general class.
    - When initializing an object, instead of trying to convert all lengths at once, it makes more sense to convert the lengths one at a time for better error handling.
    - The volume of a parallelogram cannot be determined from the lengths of its sides.
    - The `sphere` method makes more sense as a static method.

--- class2.py
+++ class2.py
@@ -1,54 +1,52 @@
-class Area:
-    def __init__(self,a,b = 1):
-        try:
-            try:
-                self.a = int(a)
-                self.b = int(b)
-            except:
-                self.a = float(a)
-                self.b = float(b)
-
-        except:
-            raise TypeError("Error Type! ")
-            exit()
-
-        
-
-    def rectangle(self):
-        return self.a * self.b 
-
-    def square(self):
-        return self.a * self.b 
-
-    def triangle(self):
-        return self.a * self.b / 2
-
-    def parallelogram(self):
-        return self.a * self.b
-    
-    def trapezoid(self,h):
-        return (self.a + self.b) * h / 2
-
-    def circle(self):
-        return (self.a ** 2) * 3.141
+import math
+import re
 
 class Volume:
-    def __init__(self,a,b,h):
-        try:
-            try:
-                self.a = int(a)
-                self.b = int(b)
-                self.h = int(h)
-            except:
-                self.a = float(a)
-                self.b = float(b)
-                self.h = float(h)
+    def __init__(self, *args):
+        args = list(args)
+        for index in range(len(args)):
+            item = args[index]
+            if type(item) in [int, float]:
+                if item < 0:
+                    raise ValueError
+            elif isinstance(item, str):
+                if re.match('[0-9]+(.[0-9]+)?$', item):
+                    if '.' in item:
+                        args[index] = float(item)
+                    else:
+                        args[index] = int(item)
+            else:
+                raise TypeError
+        self.args = args if args else [0]
+        self.dim = len(args)
+    
+    @staticmethod
+    def product(args):
+        prod = 1
+        for item in args:
+            prod *= item
+        return prod
+    
+    @staticmethod
+    def norm(val):
+        return int(val) if int(val) == val else val
 
-        except:
-            raise TypeError("Type Error! ")
-
+    @staticmethod
+    def sphere(radius, dim):
+        half = dim // 2
+        if dim % 1:
+            raise ValueError
+        if dim % 2:
+            fact = math.factorial(half) / math.factorial(dim)
+            coeff = 2 * fact * 4 ** half
+        else:
+            coeff = 1 / math.factorial(half)
+        pi_co = math.pi ** half
+        return Volume.norm(coeff * pi_co * radius ** dim)
+    
     def rectangle(self):
-        return self.a * self.b * self.h
-
-    def square(self):
-        return self.a * self.b * self.h
+        return Volume.norm(Volume.product(self.args))
+    
+    def pyramid(self):
+        vol = Volume.product(self.args) / self.dim
+        return Volume.norm(vol)
